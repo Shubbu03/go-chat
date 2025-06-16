19f76d2535ac71ff3807b93ae35cc7111ad390ce
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-16 12:22:52 +0530

    implement websocket handler

--- backend/internal/ports/websocket/ws_handler.go
+++ backend/internal/ports/websocket/ws_handler.go
@@ -1,1 +1,40 @@
 package websocket
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func ServeWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
+	}
+
+	go handleMessages(conn)
+}
+
+func handleMessages(conn *websocket.Conn) {
+	defer conn.Close()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("error reading:", err)
+			break
+		}
+
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			fmt.Println("error writing:", err)
+			break
+		}
+	}
+}
