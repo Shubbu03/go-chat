1eb07fb8137c1301f9f5fc10758a82fecfc99b4d
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-17 17:18:28 +0530

    routes init

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"gorm.io/gorm"
+)
+
+func SetupRoutes(r chi.Router, db *gorm.DB) error {
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Cookie"},
+		ExposedHeaders:   []string{"Link", "Set-Cookie"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	return nil
+}
