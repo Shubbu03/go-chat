10c9967c231be03bb02e253c37b3d67bfa4b4d76
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-18 12:31:01 +0530

    configs file

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	Port  string
+	DB_URL string
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{
+		Port:  getEnvOrDefault("PORT", "8080"),
+		DB_URL: getEnvOrDefault("DB_URL", "postgres://postgres:password@localhost:5432/chat_db?sslmode=disable"),
+	}
+
+	return cfg, nil
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
