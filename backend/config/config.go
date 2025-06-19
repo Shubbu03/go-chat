45e6b401c50dcb50ae18e42a7af52f162f6afd8a
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-19 16:37:16 +0530

    refactor config loading and database connection functions; update dependencies

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,21 +1,40 @@
 package config
 
 import (
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Config struct {
-	Port  string
+	Port   string
 	DB_URL string
 }
 
-func Load() (*Config, error) {
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
 	cfg := &Config{
-		Port:  getEnvOrDefault("PORT", "8080"),
-		DB_URL: getEnvOrDefault("DB_URL", "postgres://postgres:password@localhost:5432/chat_db?sslmode=disable"),
+		Port:   getEnvOrDefault("PORT", "8080"),
+		DB_URL: getEnvOrDefault("DB_URL", ""),
 	}
 
 	return cfg, nil
+}
+
+func ConnectToDB(cfg *Config) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(cfg.DB_URL), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func getEnvOrDefault(key, defaultValue string) string {
--- backend/go.mod
+++ backend/go.mod
@@ -4,19 +4,17 @@ go 1.24.4
 
 require (
 	github.com/go-chi/chi/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.31.0
 	gorm.io/gorm v1.30.0
-	honnef.co/go/tools v0.6.1
 )
 
 require (
-	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/tools v0.30.0 // indirect
 )
 
 require (
